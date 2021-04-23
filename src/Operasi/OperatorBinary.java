18747a86d0885a53f11d9e872b7ea176fab415ad
anastasyaokta <andreiya6@gmail.com>
2021-04-24 02:24:03 +0800

    Operator Binary assigment Operator Binary Aritmatika

--- src/Operasi/OperatorBinary.java
+++ src/Operasi/OperatorBinary.java
@@ -1,15 +1,16 @@
 package Operasi;
 
 public class OperatorBinary {
-    java.lang.String nama="anastasyaoctavianti";
+    java.lang.String nama = "anastasyaoctavianti";
     int jumlahsks = 24;
     double indeksPrestasiKumulatif = 3.54;
 
     public static void main(String[] args) {
-        System.out.println(9+3);
-        System.out.println(9-3);
-        System.out.println(9*3);
-        System.out.println(9/3);
-        System.out.println(9%3);
-        System.out.println(9%2);
+        System.out.println(9 + 3);
+        System.out.println(9 - 3);
+        System.out.println(9 * 3);
+        System.out.println(9 / 3);
+        System.out.println(9 % 3);
+        System.out.println(9 % 2);
     }
+}
