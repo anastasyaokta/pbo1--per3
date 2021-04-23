1e53cf57aef8646b7ccc6439da9d2ae22ab871ad
anastasyaokta <andreiya6@gmail.com>
2021-04-24 02:08:44 +0800

    OperatorBinary assigment

--- src/Operasi/OperatorBinary.java
+++ src/Operasi/OperatorBinary.java
@@ -1,7 +1,15 @@
 package Operasi;
 
 public class OperatorBinary {
-    java.lang.String nama="anastasya";
+    java.lang.String nama="anastasyaoctavianti";
     int jumlahsks = 24;
-    double indeksPrestasiKumulatif = 3.89;
-}
+    double indeksPrestasiKumulatif = 3.54;
+
+    public static void main(String[] args) {
+        System.out.println(9+3);
+        System.out.println(9-3);
+        System.out.println(9*3);
+        System.out.println(9/3);
+        System.out.println(9%3);
+        System.out.println(9%2);
+    }
---
+++ src/Percabangan/IfElse.java
@@ -0,0 +1,4 @@
+package Percabangan;
+
+public class IfElse {
+}
