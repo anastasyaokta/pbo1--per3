05382058dc2362002c97fb9bbb0c343f9602bc3d
anastasyaokta <andreiya6@gmail.com>
2021-04-25 10:12:08 +0800

    Switch 01

--- src/SwitchIni/switch01.java
+++ src/SwitchIni/switch01.java
@@ -2,22 +2,22 @@ package SwitchIni;
 
 public class switch01 {
     public static void main(String[] args) {
-        int jalanJalan=3;
+        int jalanJalan=5;
         switch(jalanJalan){
             case 1:
-                System.out.println("urang lapas");
+                System.out.println("Kalimantan Selatan");
                 break;
             case 2:
-                System.out.println("urang acayan");
+                System.out.println("Jawa Barat");
                 break;
             case 3:
-                System.out.println("ubat nyamuk");
+                System.out.println("Sumatera Utara");
                 break;
             case 4:
-                System.out.println("dubil dit");
+                System.out.println("Jakarta");
                 break;
             default:
-                System.out.println("mahluk halus");
+                System.out.println("Indonesia);
                 break;
         }
     }
