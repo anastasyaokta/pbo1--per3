719290b5169b9c4f71d20eb1f03eb3f56968c049
anastasyaokta <andreiya6@gmail.com>
2021-04-25 09:31:54 +0800

    Operator Binary

--- src/Operasi/OperatorBinary.java
+++ src/Operasi/OperatorBinary.java
@@ -2,6 +2,8 @@ package Operasi;
 
 public class OperatorBinary {
     java.lang.String nama = "anastasyaoctavianti";
+    java.lang.String npm = "19630975";
+    java.lang.String kelas = "FTI 4A NonReg Banjarbaru" ;
     int jumlahsks = 24;
     double indeksPrestasiKumulatif = 3.54;
 
